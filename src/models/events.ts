58438bf0503e1899cffe9ac8221e32b90667961f
TanyaKemmPere <tanya.kemmpereira@heig-vd.ch>
2023-04-20 22:05:25 +0200

    Update events.ts

--- src/models/events.ts
+++ src/models/events.ts
@@ -3,4 +3,5 @@ import { COMPONENT_BASE_NAME } from "../utils/helpers";
 export enum PlayerEvents {
   VIDEO_END = `${COMPONENT_BASE_NAME}-video-end`,
   SEQUENCE_STARTED = `${COMPONENT_BASE_NAME}-sequence-started`,
+  LAST_SEQUENCE_REACHED = `${COMPONENT_BASE_NAME}-lastSequenceReached`,
 }
